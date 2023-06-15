7de3bd82ffb921a6d93324393b6130d8c45b5991
brendi07 <116839133+brendi07@users.noreply.github.com>
2023-06-15 12:21:19 +0000

    Deploying to gh-pages from @ brendi07/goit-js-hw-09@fc219157265c02f840e175babb5d31475e1c15b0 🚀

---
+++ 01-color-switcher.e257738b.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let d=null;t.addEventListener("click",(function(a){d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,t.setAttribute("disabled",""),e.removeAttribute("disabled")}),1e3)})),e.addEventListener("click",(()=>{clearInterval(d),t.removeAttribute("disabled"),e.setAttribute("disabled","")}));
+//# sourceMappingURL=01-color-switcher.e257738b.js.map
